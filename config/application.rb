1678843a5e3fea83957cfe23e6bbfb6cad0ca47d
averageazn247 <averageazn1337@gmail.com>
2014-04-21 18:34:56 -0500

    vendor compiled assets

--- config/application.rb
+++ config/application.rb
@@ -11,13 +11,13 @@ end
 
 module MoltoSoldi
   class Application < Rails::Application
-# Settings in config/environments/* take precedence over those specified here.
+    # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
- 
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
@@ -39,7 +39,8 @@ module MoltoSoldi
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 config.time_zone = 'Central Time (US & Canada)'
-    # Enable escaping HTML in JSON. 
+    # Enable escaping HTML in JSON.
+    config.active_support.escape_html_entities_in_json = true
 
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
@@ -49,9 +50,13 @@ config.time_zone = 'Central Time (US & Canada)'
     # Enforce whitelist mode for mass assignment.
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
-    # parameters by using an attr_accessible or attr_protected declaration. 
+    # parameters by using an attr_accessible or attr_protected declaration.
+    config.active_record.whitelist_attributes = true
+
     # Enable the asset pipeline
     config.assets.enabled = true
-config.assets.initialize_on_precompile = false 
+config.assets.initialize_on_precompile = false
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
   end
 end
