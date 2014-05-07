74b1dc8acf13c16810ab4bb8c4d73e2a0420095f
averageazn247 <averageazn1337@gmail.com>
2014-05-07 12:10:23 -0500

    d

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-
+//= require bootstrap
 //= require jquery
 
 //= require jquery_ujs
-//= require bootstrap
+
 //= require jquery.ui.datepicker
 //= require underscore
 //= require gmaps/google
