283fdddd820ea2e45a4c7566532bd79ec5d037df
averageazn247 <averageazn1337@gmail.com>
2014-05-09 15:29:16 -0500

    add gmaps

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,13 +10,13 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require bootstrap
+
 //= require jquery
 
 //= require jquery_ujs
 
 //= require jquery.ui.datepicker
-
+//= require bootstrap
 //= require turbolinks
 //= require underscore
 //= require gmaps/google
