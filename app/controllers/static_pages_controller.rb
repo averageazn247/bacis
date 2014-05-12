000f21ba0ca41f32a1e205dd66487a28bdc64f09
averageazn247 <averageazn1337@gmail.com>
2014-05-11 21:50:00 -0500

    add gmaps

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,11 +11,16 @@ class StaticPagesController < ApplicationController
    @json = Gmaps4rails.build_markers(@locations) do |event, marker|
        marker.lat event.latitude
        marker.lng event.longitude
-       if city.title !=nil
+       if event.title !=nil
+         if event.desc !=nil
        marker.infowindow event.title+": \n "+event.desc
        else
-         
+         marker.infowindow  event.desc
+       end
+       else
+         if event.desc != nil
           marker.infowindow event.desc
+          end
        end
        marker.picture({
          'url' => "https://addons.cdn.mozilla.net/img/uploads/addon_icons/13/13028-64.png",
